3c30edeb9a454e81bf8942182311c50cfe463b10
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-07-16 08:05:02 +0600

    feat: implement unified client context provider layouts

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      disableTransitionOnChange={false}
+    >
+      {children}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </NextThemesProvider>
+  );
+}
